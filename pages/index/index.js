0f6913c879a1399ba557139b204553252962a87c
kmyself <dike_dik@126.com>
2019-03-31 22:09:31 +0800

    首页样式1

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,29 +5,194 @@ const app = getApp()
 Page({
   data: {
     scroll_height: 0,
-imageURL:'//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg',
-    active: 0,
-    active2: 0,
-    icon: {
-      normal:
-        'https://img.yzcdn.cn/public_files/2017/10/13/c547715be149dd3faa817e4a948b40c4.png',
-      active:
-        'https://img.yzcdn.cn/public_files/2017/10/13/793c77793db8641c4c325b7f25bf130d.png'
-    }
+    classifyList: [],
+    objs: [],
+    currentClass:0,
+    objNum:{},
+    totlePrice:0
   },
-  onLoad: function () {
+  onLoad: function() {
+
     let windowHeight = wx.getSystemInfoSync().windowHeight;
     let windowWidth = wx.getSystemInfoSync().windowWidth;
     this.setData({
-      scroll_height: windowHeight * 750 / windowWidth -  110
-    })
+      scroll_height: windowHeight * 750 / windowWidth - 110
+    });
+
+    let allData = [{
+        "classifyID": "0",
+        "classifyName": "产品1",
+        "objs": [{
+          "objID": "0",
+          "title": "2018秋季",
+          "desc": "描述信息",
+          "price": "3.00",
+          "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+          "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+          ]
+        }, {
+            "objID": "5",
+            "title": "2018秋季",
+            "desc": "描述信息",
+            "price": "3.00",
+            "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+            "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+            ]
+          }, {
+            "objID": "5",
+            "title": "2018秋季",
+            "desc": "描述信息",
+            "price": "3.00",
+            "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+            "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+            ]
+          }, {
+            "objID": "5",
+            "title": "2018秋季",
+            "desc": "描述信息",
+            "price": "3.00",
+            "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+            "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+            ]
+          }, {
+            "objID": "5",
+            "title": "2018秋季",
+            "desc": "描述信息",
+            "price": "3.00",
+            "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+            "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+            ]
+          }, {
+            "objID": "5",
+            "title": "2018秋季",
+            "desc": "描述信息",
+            "price": "3.00",
+            "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+            "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+            ]
+          }, {
+            "objID": "5",
+            "title": "2018秋季",
+            "desc": "描述信息",
+            "price": "3.00",
+            "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+            "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+            ]
+          }, {
+            "objID": "5",
+            "title": "2018秋季",
+            "desc": "描述信息",
+            "price": "3.00",
+            "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+            "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+            ]
+          }, {
+            "objID": "6",
+            "title": "2018秋季",
+            "desc": "描述信息",
+            "price": "3.00",
+            "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+            "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+            ]
+          }]
+      },
+      {
+        "classifyID": "1",
+        "classifyName": "产品2",
+        "objs": [{
+          "objID": "1",
+          "title": "2018秋季",
+          "desc": "描述信息",
+          "price": "3.00",
+          "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+          "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+          ]
+        }]
+      },
+      {
+        "classifyID": "2",
+        "classifyName": "产品1",
+        "objs": [{
+          "objID": "2",
+          "title": "2018秋季",
+          "desc": "描述信息",
+          "price": "3.00",
+          "thumbUrl": "//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg",
+          "tags": [{
+              "name": "标签"
+            },
+            {
+              "name": "标签2"
+            }
+          ]
+        }]
+      }
+    ];
+    this.setData({
+      classifyList: allData,
+      objs: allData[0].objs
+    });
   },
   onTabChange(event) {
     console.log(event.detail);
   },
   onBadgeChange(event) {
     console.log(event.detail);
-  }, 
+    this.setData({
+      currentClass: event.detail,
+      objs: this.data.classifyList[event.detail].objs
+    });
+  },
   upper(event) {
     console.log(event.detail);
   },
@@ -36,9 +201,35 @@ imageURL:'//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c5
   },
   scroll(event) {
     console.log(event.detail);
-  }, cellChoose(options){
+  },
+  cellChoose(options) {
+
+    var id = options.currentTarget.id;
+    console.log("cellChoose" + id)
+  },
+  cellBtnChoose(options) {
     console.log(options);
-    var id = options.currentTarget.dataset.id;
-    console.log(id)
+    var id = options.currentTarget.id;
+    console.log("cellBtnChoose" + id)
+  },
+  onStepperChange(event){
+    let objsN = this.data.objNum;
+    let tNum= objsN[""+this.data.currentClass];
+    if (!tNum)
+    {
+      tNum = {};
+    }
+    tNum[""+event.currentTarget.dataset.index] = event.detail;
+
+    objsN["" + this.data.currentClass] = tNum;
+
+    
+
+    this.setData({
+      objNum: objsN
+    });
+  },
+  onCommitBtn(event){
+    console.log(event);
   }
 })
